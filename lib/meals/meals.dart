3adfd2c7163ec13988d93a760ff6e6bf5ad12b7d
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-13 21:27:18 +0530

    + meals.dart

---
+++ lib/meals/meals.dart
@@ -0,0 +1,36 @@
+
+import 'package:flutter/cupertino.dart';
+
+class Meal{
+  final String id;
+  final String title;
+  final int duration;
+  final Complexity complexity;
+  final String imageUrl;
+  final Affordability affordability;
+  final List<String> categories;
+  final List<String> ingredients;
+  final List<String> steps;
+  final bool isGlutenFree;
+  final bool isLactoseFree;
+  final bool isVegan;
+  final bool isVegetarian;
+
+  const Meal({@required this.id,@required this.title,@required this.duration, this.complexity = Complexity.Simple, @required this.imageUrl,
+      this.affordability = Affordability.Affordable,@required this.categories,@required this.ingredients,@required this.steps,
+      this.isGlutenFree = false, this.isLactoseFree = false, this.isVegan = false, this.isVegetarian = false});
+
+}
+
+
+enum Complexity{
+  Simple,
+  Challenging,
+  Hard
+}
+
+enum Affordability{
+  Affordable,
+  Pricey,
+  Luxurious
+}
