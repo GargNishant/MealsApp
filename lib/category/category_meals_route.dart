a6e1ff7bdb1c054252185d84d68336b4d5efbce9
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-14 16:55:56 +0530

    *(category_meals_route.dart) ListView to builder

--- lib/category/category_meals_route.dart
+++ lib/category/category_meals_route.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mealsapp/category/category.dart';
+import 'package:mealsapp/dummy_data.dart';
+import 'package:mealsapp/meals/meals_item_widget.dart';
 
 class CategoryMealsRoute extends StatelessWidget {
   static const routeName = "/category-meals-route";
@@ -9,13 +11,19 @@ class CategoryMealsRoute extends StatelessWidget {
     final Category _category =
         ModalRoute.of(context).settings.arguments as Category;
 
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(_category.id);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(_category.title),
       ),
-      body: Center(
-        child: Text("Meals in Category"),
-      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(8.0),
+        itemBuilder: (ctx, index) => MealsItemWidget(categoryMeals[index]),
+        itemCount: categoryMeals.length,
+      )
     );
   }
 }
