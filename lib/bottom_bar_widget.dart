cdf890619c6aa17ae63ce457b449371683e26e60
Nishant Garg <nishantgarg76342@gmail.com>
2020-06-16 18:17:40 +0530

    *(bottom_bar_widget.dart) Route for BottomNavigation

---
+++ lib/bottom_bar_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:mealsapp/category/category_route.dart';
+import 'package:mealsapp/favourite/favourite_route.dart';
+
+class BottomNavigationWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigationWidget> {
+
+  List<Map<String, Object>> _pages = [
+    {'page': CategoryRoute(), 'title': "Daily Meals"},
+    {'page': FavouriteRoute(), "title": "Favourites"}
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() => _selectedPageIndex = index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          onTap: _selectPage,
+          currentIndex: _selectedPageIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              title: Text("Categories"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              title: Text("Favourites"),
+            ),
+          ]),
+    );
+  }
+}
